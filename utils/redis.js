cc2c25f910b0382ec9f994d58cb1e67af1359c22
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-12-05 15:05:30 +0000

    eslint

--- utils/redis.js
+++ utils/redis.js
@@ -1,44 +1,43 @@
 const redis = require('redis');
 
-
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (error) => {
-            console.error(error);
-        });
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (error, value) => {
-                if (error) return reject(error);
-                return resolve(value);
-            });
-        });
-    }
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) return reject(error);
+        return resolve(value);
+      });
+    });
+  }
 
-    async set(key, value, duration) {
-        return new Promise((resolve, reject) => {
-            this.client.setex(key, duration, value, (error, reply) => {
-                if (error) return reject(error);
-                return resolve(reply);
-            });
-        });
-    }
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, reply) => {
+        if (error) return reject(error);
+        return resolve(reply);
+      });
+    });
+  }
 
-    async del(key) {
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (error, reply) => {
-                if (error) return reject(error);
-                return resolve(reply);
-            });
-        });
-    }
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) return reject(error);
+        return resolve(reply);
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
