f07144518b709e3093e24b457cbad8f636653c3e
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-12-05 14:16:40 +0000

    tasks 0

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+const redis = require('redis');
+
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.client.on('error', (error) => {
+            console.error(error);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (error, value) => {
+                if (error) return reject(error);
+                return resolve(value);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (error, reply) => {
+                if (error) return reject(error);
+                return resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (error, reply) => {
+                if (error) return reject(error);
+                return resolve(reply);
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
